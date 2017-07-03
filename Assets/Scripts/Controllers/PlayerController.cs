fa097eaf643f12b138d67d43c41f8893bca2d6d0
Thanadev <thomas.lambert33720@gmail.com>
2017-07-04 00:34:06 +0200

    Added player movement

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -14,7 +14,13 @@ public class PlayerController : MonoBehaviour {
 	
 	void Update () {
 		Vector3 nextVelocity = new Vector3 (Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		
+		nextVelocity = transform.TransformDirection (nextVelocity);
 		rb.velocity = nextVelocity;
+
+		Vector3 nextEuler = transform.rotation.eulerAngles;
+		nextEuler.y += Input.GetAxisRaw ("Mouse X");
+		nextEuler.x += Input.GetAxisRaw ("Mouse Y") * -1;
+
+		transform.rotation = Quaternion.Euler (nextEuler);
 	}
 }
