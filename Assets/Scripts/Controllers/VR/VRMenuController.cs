bd32766f5bc3933ec0331b89288f25a2af369042
Thanadev <thomas.lambert33720@gmail.com>
2017-07-04 21:18:58 +0200

    Added menu + graphics

---
+++ Assets/Scripts/Controllers/VR/VRMenuController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRMenuController : MonoBehaviour {
+
+	private SteamVR_TrackedController controller;
+
+	// Use this for initialization
+	void Awake () {
+		controller.TriggerClicked += TriggerPressedHandler;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void TriggerPressedHandler (object sender, ClickedEventArgs args) {
+		RaycastHit hit;
+
+		if (Physics.Raycast (transform.position, transform.forward, out hit)) {
+			VRMenuPlayButton playButton = hit.collider.GetComponent<VRMenuPlayButton> ();
+
+			if (playButton != null) {
+				playButton.StartGame ();
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Controllers/VR/VRMenuPlayButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VRMenuPlayButton : MonoBehaviour {
+
+	public void StartGame () {
+		SceneManager.LoadScene ("main");
+	}
+}
