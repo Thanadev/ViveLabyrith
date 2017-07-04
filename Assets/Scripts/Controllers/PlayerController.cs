51df4771071023da643d0d7e0ee08621c6099d8d
Thanadev <thomas.lambert33720@gmail.com>
2017-07-04 10:55:45 +0200

    Improved player movement

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour {
 	public GameObject cameraObject;
+	public float speedFactor = 100;
 
 	Rigidbody rb;
 
@@ -15,7 +16,7 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 		Vector3 nextVelocity = new Vector3 (Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 		nextVelocity = transform.TransformDirection (nextVelocity);
-		rb.velocity = nextVelocity;
+		rb.velocity = nextVelocity * Time.deltaTime * speedFactor;
 
 		Vector3 nextEuler = transform.rotation.eulerAngles;
 		nextEuler.y += Input.GetAxisRaw ("Mouse X");
