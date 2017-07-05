ea8822e5f370c68532478ce45c441a9e896b0938
User-VR <User-VR@INFO-VEGETA.iut.bx1>
2017-07-05 17:39:35 +0200

    Menuplay

--- Assets/Scripts/Controllers/VR/VRMenuPlayButton.cs
+++ Assets/Scripts/Controllers/VR/VRMenuPlayButton.cs
@@ -1,11 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class VRMenuPlayButton : MonoBehaviour {
 
+    public float secondsBeforeActivation = 1f;
+
+    void Awake ()
+    {
+        GetComponent<BoxCollider>().enabled = false;
+        GetComponent<Button>().interactable = false;
+    }
+
+    void Start ()
+    {
+        StartCoroutine(TimerForActivation());
+    }
+
 	public void StartGame () {
 		SceneManager.LoadScene ("main");
 	}
+
+    private IEnumerator TimerForActivation ()
+    {
+        yield return new WaitForSeconds(secondsBeforeActivation);
+
+        GetComponent<BoxCollider>().enabled = true;
+        GetComponent<Button>().interactable = true;
+    }
 }
